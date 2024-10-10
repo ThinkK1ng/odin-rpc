be13c653022ab4df253225d602e444a0e4cefe84
ThinkK1ng <cla.givens@gmail.com>
2024-10-09 21:53:05 -0500

    create getComputerChoice function created a function to get the computer's choice in rock, paper, scissors. Tested it in the console.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+// Creating variables
+const choices = ["rock", "paper", "scissors"]
+
+// creating get computer choice function
+function getComputerChoice(arr) {
+    const randomChoice = Math.floor(Math.random() * arr.length);
+    return arr[randomChoice];
+}
+// creating variable for choosing rock, paper, or scissors
+const randomComputerChoice = getComputerChoice(choices);
+console.log(randomComputerChoice);
+
+function getHumanChoice() {
+    
+}
