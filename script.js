be288049e274bf8528c77a8891a6e78a6c04b573
ThinkK1ng <cla.givens@gmail.com>
2024-10-10 21:33:25 -0500

    Add get human choice function Added a play game button to the html to prompt the user with choices for rock paper scissors. When they make a selection, it logs it to the console. If they put the wrong word or don't make a choice, a second promp appears telling them they made a mistake and need to input one of the three choices.

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 // console.log('Hello World');
 
 // Creating variables
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
 
 // creating get computer choice function
 function getComputerChoice(arr) {
-    const randomChoice = Math.floor(Math.random() * arr.length);
-    return arr[randomChoice];
+  const randomChoice = Math.floor(Math.random() * arr.length);
+  return arr[randomChoice];
 }
 // creating variable for choosing rock, paper, or scissors
 const randomComputerChoice = getComputerChoice(choices);
 console.log(randomComputerChoice);
 
-function getHumanChoice() {
-    
+// function to run on "Play Game" button click
+function getHumanChoice(askChoice) {
+  // Creating a variable with a prompt as a value for the user choice.
+  askChoice = prompt(
+    "Can you beat the computer!? Input rock, paper, or scissors to test your luck!"
+  );
+  console.log("button clicked");
+  if (askChoice === choices[0]) {
+      console.log("rock");
+  } else if (askChoice === choices[1]) {
+      console.log("paper");
+  } else if (askChoice === choices[2]) {
+      console.log("scissors");
+  } else {
+    // run this code if the user doesn't make a selection or inputs a different word
+      let retry = prompt("You haven't made a selection! Input rock, paper, or scissors.");
+      if (retry === choices[0]) {
+        console.log("rock");
+      } else if (retry === choices[1]) {
+        console.log("paper");
+    } else if (retry === choices[2]) {
+        console.log("scissors");
+    }
+  }
 }
+
+// creating button variable
+button = document.getElementById("bttn");
+
+// creating event listener for executing the getHumanChoice function
+button.addEventListener("click", getHumanChoice);
